1076a341a42f3e56d85bf20c9e6e26e2bbc317c6
Marina Berdnikova <berdni4ka@rambler.ru>
2023-04-17 16:29:52 +0300

    Add task 1

---
+++ src/Task_1.java
@@ -0,0 +1,39 @@
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Task_1 {
+
+  static Scanner scanner = new Scanner(System.in);
+  public static void main(String[] args) {
+    Notebook apple = new Notebook("apple", 16, 512, "macos", "silver");
+    Notebook acer = new Notebook("acer", 4, 512, "widows", "black");
+    Notebook hp = new Notebook("hp", 8, 1024, "widows", "white");
+    Map <String, Integer> criteronChoose = criterionNotebook();
+    int ramChoose = criteronChoose.get("Ram");
+    int ssdChoose = criteronChoose.get("Ssd");
+
+
+    System.out.println("Результаты поиска: ");
+    if(ramChoose <= apple.getRam() && ssdChoose <= apple.getSsd()) {
+      System.out.println(apple);
+    }
+    if(ramChoose <= acer.getRam() && ssdChoose <= acer.getSsd()) {
+      System.out.println(acer);
+    }
+    if(ramChoose <= hp.getRam() && ssdChoose <= hp.getSsd()) {
+      System.out.println(hp);
+    }
+  }
+  private static Map<String, Integer> criterionNotebook() {
+    Map <String, Integer> choose = new LinkedHashMap<>();
+    System.out.print("Введиите объем необходимой оперативной памяти: ");
+    int ram = scanner.nextInt();
+    System.out.print("Введиите необходимый объем жестого диска: ");
+    int ssd = scanner.nextInt();
+    choose.put("Ram", ram);
+    choose.put("Ssd", ssd);
+    return choose;
+  }
+}
